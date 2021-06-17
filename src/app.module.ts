8ad011cb0eda49977e2207c601affa21c6a7c46f
shadhin <shadhin.int@gmail.com>
2021-06-17 16:11:31 +0600

    added task controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TaksController } from './taks/taks.controller';
 
 @Module({
   imports: [TasksModule],
+  controllers: [TaksController],
 })
 export class AppModule {}
---
+++ src/taks/taks.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('taks')
+export class TaksController {}
