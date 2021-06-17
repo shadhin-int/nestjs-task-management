37a00f280dec2221fd6a23f99a23c24e1743cb46
shadhin <shadhin.int@gmail.com>
2021-06-17 18:00:22 +0600

    project refactored

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TasksController } from './tasks/tasks.controller';
+// import { TasksService } from './tasks/tasks.service';
 
 @Module({
   imports: [TasksModule],
-  controllers: [TasksController],
 })
 export class AppModule {}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks = { task: 'There is no task' };
+
+  getAllTasks() {
+    return this.tasks;
+  }
+}
