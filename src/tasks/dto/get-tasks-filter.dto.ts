87205c37f5b152995cc31cd9b79fbbb4eff4d1d3
shadhin <shadhin.int@gmail.com>
2021-06-22 15:33:29 +0600

    added validation on task filter

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
 import { TaskStatus } from '../tasks.model';
+import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsIn([TaskStatus.OPEN, TaskStatus.OPEN, TaskStatus.IN_PROGRESS])
   status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
